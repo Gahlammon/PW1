4387f862bd000e181be029a16978cae08d055b14
Gahlammon <adam@dachtera.pl>
2023-04-17 08:17:53 +0200

    Remove old files and setup new form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -3,14 +3,14 @@
     partial class Form1
     {
         /// <summary>
-        /// Wymagana zmienna projektanta.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Wyczyść wszystkie używane zasoby.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,192 +20,55 @@
             base.Dispose(disposing);
         }
 
-        #region Kod generowany przez Projektanta formularzy systemu Windows
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
-        /// jej zawartości w edytorze kodu.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Location = new System.Drawing.Point(12, 146);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(0, 0);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 23);
-            this.label1.TabIndex = 0;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2,
-            this.Column3});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(367, 150);
-            this.dataGridView1.TabIndex = 3;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
-            // Column1
-            // 
-            this.Column1.HeaderText = "Nazwa";
-            this.Column1.Name = "Column1";
-            // 
-            // Column2
-            // 
-            this.Column2.HeaderText = "Narodowość";
-            this.Column2.Name = "Column2";
-            // 
-            // Column3
-            // 
-            this.Column3.HeaderText = "Rola";
-            this.Column3.Name = "Column3";
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(13, 169);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(118, 33);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Dodaj";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(137, 169);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(118, 33);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "Usuń";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(137, 210);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(118, 33);
-            this.button5.TabIndex = 7;
-            this.button5.Text = "Wczytaj .csv";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(13, 210);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(118, 33);
-            this.button6.TabIndex = 6;
-            this.button6.Text = "Zapisz .csv";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(261, 169);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(118, 33);
-            this.button7.TabIndex = 8;
-            this.button7.Text = "Wyszukaj";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(137, 253);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(118, 33);
-            this.button8.TabIndex = 10;
-            this.button8.Text = "Wczytaj .xml";
-            this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
-            // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(13, 253);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(118, 33);
-            this.button9.TabIndex = 9;
-            this.button9.Text = "Zapisz .xml";
-            this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "wczytaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(390, 298);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(312, 181);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "PROJEKT";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button9;
     }
 }
-
--- Form1.cs
+++ Form1.cs
@@ -7,151 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
-using System.Xml;
-using System.Xml.Serialization;
 
 namespace Project1
 {
-    [Serializable]
     public partial class Form1 : Form
     {
-        private List<Hero> heroes;
-        private string[] avaiableRoles = { "Tank", "Natarcie", "Wsparcie" };
-
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void removeFromList(int index)
-        {
-            heroes.RemoveAt(index);
-            dataGridView1.Rows.RemoveAt(index);
-        }
-        private void addToList(string name, string nationality, string role)
-        {
-            dataGridView1.Rows.Add(name, nationality, role);
-        }
-        private void recreateDataGrid()
-        {
-            while (dataGridView1.Rows.Count > 0)
-            {
-                dataGridView1.Rows.RemoveAt(0);
-            }
-            for (int i = 0; i < heroes.Count; i++)
-            {
-                addToList(heroes[i].name, heroes[i].nationality, heroes[i].role);
-            }
-        }
-        public void newRecord(string name, string nationality, string role)
-        {
-            bool correctRole = false;
-            for (int i = 0; i < avaiableRoles.Length; i++)
-            {
-                if (role == avaiableRoles[i])
-                {
-                    correctRole = true;
-                    break;
-                }
-            }
-            if (correctRole)
-            {
-                heroes.Add(new Hero(name, nationality, role));
-                addToList(name, nationality, role);
-            }
-        }
-        private void saveDataCsv(string path)
-        {
-            string output = "";
-            for (int i = 0; i < heroes.Count; i++)
-            {
-                output += heroes[i].name + "," + heroes[i].nationality + "," + heroes[i].role + ",";
-            }
-            File.WriteAllText(path, output);
-        }
-        private void loadDataCsv(string path)
-        {
-            string input = File.ReadAllText(path);
-            string name;
-            string nationality;
-            string role;
-            heroes = new List<Hero>();
-            while (input != "")
-            {
-                name = input.Substring(0,input.IndexOf(','));
-                input = input.Substring(input.IndexOf(',') + 1, input.Length - input.IndexOf(',') - 1);
-                nationality = input.Substring(0, input.IndexOf(','));
-                input = input.Substring(input.IndexOf(',') + 1, input.Length - input.IndexOf(',') - 1);
-                role = input.Substring(0, input.IndexOf(','));
-                input = input.Substring(input.IndexOf(',') + 1, input.Length - input.IndexOf(',') - 1);
-                heroes.Add(new Hero(name, nationality, role));
-            }
-            recreateDataGrid();
-        }
-        private void loadDataXml(string path)
-        {
-            XmlSerializer serializer = new XmlSerializer(typeof(List<Hero>));
-            StreamReader reader = new StreamReader(path);
-            heroes = (List<Hero>)serializer.Deserialize(reader);
-            reader.Close();
-            recreateDataGrid();
-        }
-        private void saveDataXml(string path)
-        {
-            XmlSerializer serializer = new XmlSerializer(typeof(List<Hero>));
-            StreamWriter writer = new StreamWriter(path);
-            serializer.Serialize(writer, heroes);
-            writer.Close();
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
-            Form2 creator = new Form2(this);
-            creator.ShowDialog();
-        }
 
-        private void button4_Click(object sender, EventArgs e)
-        {
-            if (dataGridView1.Rows.Count > 0)
-            {
-                while (dataGridView1.SelectedRows.Count > 1)
-                {
-                    removeFromList(dataGridView1.SelectedRows[0].Index);
-                }
-                removeFromList(dataGridView1.SelectedRows[0].Index);
-            }
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            saveDataCsv("Data.csv");
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            loadDataCsv("Data.csv");
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            Form3 creator = new Form3(heroes);
-            creator.ShowDialog();
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-            saveDataXml("Data.xml");
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            loadDataXml("Data.xml");
         }
     }
 }
