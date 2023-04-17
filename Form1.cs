126967db00d81bd07e6203d8cdae0b122fa74e91
Gahlammon <adam@dachtera.pl>
2023-04-17 08:41:37 +0200

    Add image loading and create proper files

--- Form1.cs
+++ Form1.cs
@@ -7,19 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Project1
 {
     public partial class Form1 : Form
     {
+        Bitmap image;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void updateImage()
+        {
+            pictureBox1.Image = image;
+        }
+        private void loadImage(string path)
+        {
+            Image input = Image.FromStream(File.Open(path,FileMode.Open));
+            image = new Bitmap(input);
+            updateImage();
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-
+            loadImage("Image.png");
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
