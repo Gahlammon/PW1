24b2931ee86d508a50a7388e535ec740d7f37e50
Gahlammon <adam@dachtera.pl>
2023-04-24 08:58:51 +0200

    Setup form1 and form2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,112 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button3movement = new System.Windows.Forms.Button();
-            this.button2movement = new System.Windows.Forms.Button();
-            this.button1movement = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // comboBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(222, 21);
+            this.comboBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 146);
+            this.button1.Location = new System.Drawing.Point(12, 40);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.Size = new System.Drawing.Size(222, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "wczytaj";
+            this.button1.Text = "OTWÓRZ";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(147, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(153, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "zieleń";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(147, 146);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(153, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "negatyw";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button3movement
-            // 
-            this.button3movement.Location = new System.Drawing.Point(147, 116);
-            this.button3movement.Name = "button3movement";
-            this.button3movement.Size = new System.Drawing.Size(153, 23);
-            this.button3movement.TabIndex = 2;
-            this.button3movement.Text = "odbij pionowo";
-            this.button3movement.UseVisualStyleBackColor = true;
-            this.button3movement.Click += new System.EventHandler(this.button3movement_Click);
-            // 
-            // button2movement
-            // 
-            this.button2movement.Location = new System.Drawing.Point(147, 87);
-            this.button2movement.Name = "button2movement";
-            this.button2movement.Size = new System.Drawing.Size(153, 23);
-            this.button2movement.TabIndex = 3;
-            this.button2movement.Text = "odbij poziomo";
-            this.button2movement.UseVisualStyleBackColor = true;
-            this.button2movement.Click += new System.EventHandler(this.button2movement_Click);
-            // 
-            // button1movement
-            // 
-            this.button1movement.Location = new System.Drawing.Point(146, 58);
-            this.button1movement.Name = "button1movement";
-            this.button1movement.Size = new System.Drawing.Size(153, 23);
-            this.button1movement.TabIndex = 4;
-            this.button1movement.Text = "obróć";
-            this.button1movement.UseVisualStyleBackColor = true;
-            this.button1movement.Click += new System.EventHandler(this.button1movement_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(312, 181);
-            this.Controls.Add(this.button1movement);
-            this.Controls.Add(this.button2movement);
-            this.Controls.Add(this.button3movement);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(247, 77);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button3movement;
-        private System.Windows.Forms.Button button2movement;
-        private System.Windows.Forms.Button button1movement;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -7,15 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 
 namespace Project1
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
