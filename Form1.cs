d3685c356b250c2338464d73bd227e85ff15c43e
Gosia:) <msteperska@gmail.com>
2023-06-07 16:15:25 +0200

    Add GUI

--- Form1.cs
+++ Form1.cs
@@ -14,9 +14,41 @@ namespace Project1
 {
     public partial class Form1 : Form
     {
+        int card_count = 26;
         public Form1()
         {
             InitializeComponent();
+            panel1.Visible = false;
+            panel2.Visible = false;
+            listView_BOT.Visible = false;
+        }
+
+        private void War_button_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            panel2.Visible = false;
+
+        }
+
+        private void W_card_view_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BJ_Button_Click(object sender, EventArgs e)
+        {
+            panel2.Visible = true;
+            //panel1.Visible = false;
+        }
+
+        private void W_card_button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BJ_stop_button_Click(object sender, EventArgs e)
+        {
+            listView_BOT.Visible = true;
         }
     }
 }
