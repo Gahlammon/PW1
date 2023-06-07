bd3124da64d59223edafd3d8cf72130f9341cc47
Gahlammon <adam@dachtera.pl>
2023-06-07 16:16:53 +0200

    Add Card and CardDeck classes

---
+++ Card.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Card
+    {
+        public enum Color
+        {
+            Clubs,
+            Diamonds,
+            Hearts,
+            Spades
+        }
+        public enum Value
+        {
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack,
+            Queen,
+            King,
+            Ace
+        }
+        private Color cardColor;
+        private Value cardValue;
+        public Card(Color newCardColor, Value newCardValue)
+        {
+            cardColor = newCardColor;
+            cardValue = newCardValue;
+        }
+        public Color GetColor()
+        {
+            return cardColor;
+        }
+        public Value GetValue()
+        {
+            return cardValue;
+        }
+        public int GetBlackJackValue()
+        {
+            return (0);
+        }
+        public int GetWarValue()
+        {
+            return (0);
+        }
+    }
+}
---
+++ CardDeck.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class CardDeck
+    {
+        private Queue<Card> Cards = new Queue<Card>();
+
+        public int CountCards()
+        {
+            return Cards.Count;
+        }
+        public void PutCardAtBottom(Card newCard)
+        {
+            Cards.Enqueue(newCard);
+        }
+        public Card DrawCard()
+        {
+            if(Cards.Count > 0)
+            {
+                return Cards.Dequeue();
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void CreateNewDeck()
+        {
+
+        }
+        public CardDeck()
+        {
+            CreateNewDeck();
+        }
+    }
+}
